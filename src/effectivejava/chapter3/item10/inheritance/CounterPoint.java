b7bedf864c90aee2ae1527610be78d6cb6f9e4db
Mat <klchan.358@gmail.com>
2024-07-27 02:00:26 +0800

    Formatting

--- src/effectivejava/chapter3/item10/inheritance/CounterPoint.java
+++ src/effectivejava/chapter3/item10/inheritance/CounterPoint.java
@@ -1,6 +1,6 @@
 package effectivejava.chapter3.item10.inheritance;
-import effectivejava.chapter3.item10.Point;
 
+import effectivejava.chapter3.item10.Point;
 import java.util.concurrent.atomic.*;
 
 // Trivial subclass of Point - doesn't add a value component (Page 43)
